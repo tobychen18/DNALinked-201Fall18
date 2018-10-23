f3bfa64aad30c7ac52964a7cbc657891527cfd50
toby.chen@duke.edu <tobychen00@Tobys-MacBook-Pro.local>
2018-10-23 11:57:08 -0400

    reverse final

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -51,28 +51,37 @@ public class LinkStrand implements IDnaStrand {
 	
 	@Override
 	public IDnaStrand reverse() {
+		
 		StringBuilder reversedStringBuilder = new StringBuilder(); //make a new string builder
 		reversedStringBuilder.append(this.toString()).reverse(); //reverse all the strings in this list
 		ArrayList<String> reversedStringsInSameOrder = new ArrayList<String>(); //make an arrayList to store the reversed strings but not in the correct order yet
+		
 		Node list = myFirst; //make a new node pointer
 		int pointer = 0; //make a pointerValue
 		int size = reversedStringBuilder.length(); //get the size of the string we reversed
+		
 		while(list != null) { //for all the things in list
 			
-			String nodeReversedString = reversedStringBuilder.substring(size-(list.info.length()+pointer), size - pointer).toString();
-			reversedStringsInSameOrder.add(nodeReversedString);
+			String nodeReversedString = reversedStringBuilder.substring(size-(list.info.length()+pointer), size - pointer).toString(); //this is the string we want from the reversed string builder
+			//you go from reverse aka the back of the reversed string to get the first node's string and so on, you go from the back 
 			
-			pointer += list.info.length();
-			list = list.next;
+			reversedStringsInSameOrder.add(nodeReversedString); //add this reversed string to list
+			
+			pointer += list.info.length(); //update pointer
+			list = list.next; //update list
 		}
+		
+		/*
 		if(reversedStringsInSameOrder.size() == 0) {
-			return null;
+			return null; //if there's nothing then it's null
 		}
-		LinkStrand reversed = new LinkStrand(reversedStringsInSameOrder.get(reversedStringsInSameOrder.size()-1).toString());
-		for(int i = reversedStringsInSameOrder.size()-2; i >= 0; i--) {
-			reversed.append(reversedStringsInSameOrder.get(i).toString());
+		*/
+		
+		LinkStrand reversed = new LinkStrand(reversedStringsInSameOrder.get(reversedStringsInSameOrder.size()-1).toString()); //initialize a new LinkStrand with the last element of the arrayList aka the last node
+		for(int i = reversedStringsInSameOrder.size()-2; i >= 0; i--) { //go backwards so you add the nodes in reverse order since we are reversing
+			reversed.append(reversedStringsInSameOrder.get(i).toString()); //add the nodes in reverse order such that order 1,2,3,4 is reversed to 4,3,2,1
 		}
-		return reversed;
+		return reversed; //return the new reversed linkstrand
 	}
 	@Override
 	public int getAppendCount() {
