3d16d40073cdae280def1bc7d8c57e536014b92c
toby.chen@duke.edu <tobychen00@Tobys-MacBook-Pro.local>
2018-10-23 11:28:01 -0400

    reverse try3

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -27,7 +27,7 @@ public class LinkStrand implements IDnaStrand {
 	@Override 
 	public void initialize(String source) {
 		myFirst = new Node(source);
-		myFirst = myLast;
+		myLast = myFirst;
 		mySize = source.length();
 		myAppends = 0;
 				
