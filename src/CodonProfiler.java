f22f8be972a54e9df9eb125a0520174b4d74874e
toby.chen@duke.edu <tobychen00@SamvedsiPhone2.wired.duke.local>
2018-10-23 23:52:50 -0400

    final with documentation

--- src/CodonProfiler.java
+++ src/CodonProfiler.java
@@ -7,46 +7,47 @@ public class CodonProfiler {
 	 * in a strand of DNA. Return int[] such that int[k] is number
 	 * of occurrences of codons[k] in strand. Strand codons can start
 	 * at all valid indexes that are multiples of 3: 0, 3, 6, 9, 12, ...
-	 * @param strand is DNA to be analyzed for codon occurrences.
-	 * @param codons is an array of strings, each has three characters
-	 * @return int[] such that int[k] is number of occurrences of codons[k] in 
-	 * strand. 
+	 * maps all the occurrences of all different codons  using iterator
+	 * then uses this map to see how many instances of the codons we want to look at are
+	 * @param strand is DNA to be analyzed for codon occurrences. The strand of DNA we are using.
+	 * @param codons is an array of strings, each has three characters. Gives an arrayList of Strings that we want to find the codons for
+	 * @return int[] such that int[k] is number of occurrences of codons[k] in strand. 
 	 */
 	public int[] getCodonProfile(IDnaStrand strand, String[] codons) {
-		HashMap<String,Integer> map = new HashMap<>();
-		int[] ret = new int[codons.length];
-		Iterator<Character> iter = strand.iterator();
-		while (iter.hasNext()) {
-			char a = iter.next();
+		HashMap<String,Integer> map = new HashMap<>(); //create a new hashMap of strings keys, and integer values that will map codons and the number of occurences of the codons
+		int[] ret = new int[codons.length]; //create an array that is the same length of codon array to find # of instances of the codons that matches up to its position in codons and ret
+		Iterator<Character> iter = strand.iterator(); //create an iterator of the dna strand we are looking at
+		while (iter.hasNext()) { // as long as this DNA strand still has characters
+			char a = iter.next(); //char a is the next letter 
 			char b = 'z';           // not part of any real codon
-			char c = 'z';
-			if (iter.hasNext()) {
+			char c = 'z';			//not part of any real codon
+			if (iter.hasNext()) { //if DNA has another character assign it to b
 				b = iter.next();
 			}
-			if (iter.hasNext()) {
+			if (iter.hasNext()) { //if DNA has even another character assign it to c
 				c = iter.next();
 			}
-			if(b != 'z' && c != 'z') {
+			if(b != 'z' && c != 'z') { //if b and c got assigned to possible values in the DNA aka the DNA strand had enough characters to update b and c
 
-				String codon = "" + a + b + c;
-				if(!map.containsKey(codon)) {
-					map.put(codon, 1);
+				String codon = "" + a + b + c; //the codon is the next 3 characters
+				if(!map.containsKey(codon)) { //if the map doesn't contain the codon
+					map.put(codon, 1); //put the codon into the map and put that its the first occurrence
 				}
 				else {
-					map.put(codon, map.get(codon)+1);
+					map.put(codon, map.get(codon)+1); //add one to the occurrences of this codon
 				}
 			}
 		}
-		for( int i = 0; i < codons.length; i++) {
-			if(map.containsKey(codons[i])) {
-				ret[i] = map.get(codons[i]);
+		for( int i = 0; i < codons.length; i++) { //traverse the codons array
+			if(map.containsKey(codons[i])) { //if the map contains the codon at index i, we are looking at in codons array
+				ret[i] = map.get(codons[i]); //put the amount of occurrences of that codon into ret at the same index i
 			}
-			else {
-				ret[i] = 0;
+			else { //otherwise the codon is not in our map/dna strand
+				ret[i] = 0;  //so set that index to 0
 			}
 		}
 
-		return ret;
+		return ret; //return the occurrences of codons that indexes match up 
 	}
 }
 
--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,3 +1,7 @@
+/*
+ * use a linked list to represent a more efficient DNA strand
+ * @author Toby Chen tc248
+ */
 import java.util.ArrayList;
 
 public class LinkStrand implements IDnaStrand {
@@ -9,25 +13,44 @@ public class LinkStrand implements IDnaStrand {
 	private int myIndex;
 	private Node myCurrent;
 
-
+	
+	/*
+	 * Internal node class used for having an internal linked list for linked Strand
+	 */
 	private class Node {
-		String info;
-		Node next;
+		String info; //the info of the node
+		Node next; //the next node that this node is pointing at
+		/*
+		 * Make a node that houses a string and points to null
+		 * @param s string that the node will house
+		 */
 		public Node(String s) {
 			info = s;
 			next = null;
 		}
 	}
 
-
-
+/*
+ * Default constructor that makes this DNALinkStrand's string empty
+ */
 	public LinkStrand() {
 		this("");
 	}
+	
+	/*
+	 * Constructor that takes in a string str as the DNA strand and initializes the rest of the fields
+	 *
+	 */
 	public LinkStrand(String str) {
 		initialize(str);
 	}
-
+	
+	/*
+	 * Initialize by copying the given strand as the DNA strand we will be using as our first node as well as resetting all other fields.
+	 * set our nodes as initialize will create the one and only node in the DNA strand at the moment
+	 * @param source is the string used to initialize this strand
+ * @see IDnaStrand#initialize(java.lang.String) for more info
+ */
 	@Override 
 	public void initialize(String source) {
 		myFirst = new Node(source);
@@ -39,14 +62,29 @@ public class LinkStrand implements IDnaStrand {
 		myCurrent = myFirst;
 	}
 
+	/**
+	 * Returns a new LinkStrand with DNA of string source and only 1 node
+	 * @param the dna string that we want to return a new linkstrand with
+	 * @return new linkStrand with one node of DNA that is given in the parameter
+	 */
 	@Override 
 	public IDnaStrand getInstance(String source) {
 		return new LinkStrand(source);
 	}
+	
+	/*
+	 *finds the size of this DNA linkStrand's DNA strand of all nodes combined
+	 *@return size of myDNA strand
+	 */
 	@Override
 	public long size() {
 		return mySize;
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see IDnaStrand#append(java.lang.String)
+	 */
 	@Override
 	public IDnaStrand append(String dna) {
 		myLast.next = new Node(dna);
@@ -56,6 +94,10 @@ public class LinkStrand implements IDnaStrand {
 		return this;
 	}
 
+	/*
+	 * Reverse the DNA strand string and it's corresponding nodes without mutating this linkStrand but by returning a new linkedstrand
+	 * @return a new LinkedStrand that has its nodes and its nodes values reverse, aka reversing the entire linked strand
+	 */
 	@Override
 	public IDnaStrand reverse() {
 
@@ -90,39 +132,61 @@ public class LinkStrand implements IDnaStrand {
 		}
 		return reversed; //return the new reversed linkstrand
 	}
+	
+	/*
+	 * return the number of times you have added a new node aka added new parts of DNA
+	 * @return the number of times we called appends
+	 */
 	@Override
 	public int getAppendCount() {
 		return myAppends;
 	}
+	
+	/*
+	 * Returns a character at a specified index of the DNA linked strand but with this linked list charAt will be more efficient 
+	 * by starting the search from the end of our previous search, so we don't have to start from the beginning each time or if necessary start from the beginning
+	 * start the search from end of our previous search using local variables
+	 * @param the index that we want to find the character at
+	 * @return the character at the given index
+	 * @throws IndexOutOfBoundsException if index < 0 or index >= size() aka index is not possible for our DNA strand
+	*/
+	
 	@Override
-	public char charAt(int index) {
-		if(index >= this.size() || index< 0) {
-			throw new IndexOutOfBoundsException("not a valid index");
+	public char charAt(int index) { 
+		if(index >= this.size() || index< 0) { //if the index is not in the DNA strand aka not possible
+			throw new IndexOutOfBoundsException("not a valid index"); //throw an exception that says this index is not a valid index
 		}
-		if(myIndex>index || myCurrent == null) {
-			myCurrent = myFirst;
+		if(myIndex>index || myCurrent == null) { //if myIndex is more than index then start from the beginning rather than starting from myIndex because you have to start search from beginning or if you are at the end and its the last node then reset to start from the beginning
+			myCurrent = myFirst; //reset to the beginning
 			myLocalIndex = 0;
 			myIndex = 0;
 		}
-		while(myIndex != index) {
-			myIndex++;
+		while(myIndex != index) { //if myIndex is not index
+			myIndex++; //then add one to index and myLocal index
 			myLocalIndex++; //got to do this before you test the if statement because if not you could get an index out of bounds error
-			if(myLocalIndex >= myCurrent.info.length())
+			if(myLocalIndex >= myCurrent.info.length()) //if my local index is more than the size of the node, go to the next node because you have traversed the whole node
 			{
-				myCurrent = myCurrent.next;
-				myLocalIndex = 0;
+				myCurrent = myCurrent.next; //go to the next node
+				myLocalIndex = 0; //reset my local index because now you are looking at the first thing in the next node
 			}
 		}
-		return myCurrent.info.charAt(myLocalIndex);
+		//loop ends when myIndex = index and myCurrent and myLocal index point at the char you want
+		return myCurrent.info.charAt(myLocalIndex); //return the character at the index of the string in the node
 	}
+	
+	/*
+	 * return the string of the DNA strand, linking the linkedStrand node information together
+	 * Use stringBuilder to be more efficient to add the strings together in the order of the linkedList nodes
+	 * @return the String of all the nodes in linkedList combined
+	 */
 
 	@Override
 	public String toString() {
-		StringBuilder str = new StringBuilder(); 
-		Node list = myFirst;
-		while(list != null) {
-			str.append(list.info);
-			list = list.next;
+		StringBuilder str = new StringBuilder();  //create a new string builder
+		Node list = myFirst; //list pointer to myFirst
+		while(list != null) { //keep going till the end of the linkedList
+			str.append(list.info); //add the info of the next node
+			list = list.next; //go to the nextNode
 		}
 		return str.toString();
 	}
